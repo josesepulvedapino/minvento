46845776dfe1797f9a29236faa04f5688dd9e6d0
Jose Sepulveda <josealejandrosepulvedapino@gmail.com>
2024-01-14 13:41:46 -0300

    feat: cambio de componente al seleccionar opcion menu

--- src/components/lateral-dashboard/lateral-dashboard.component.ts
+++ src/components/lateral-dashboard/lateral-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MenuServiceService } from '../../app/services/menu-service.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lateral-dashboard',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './lateral-dashboard.component.scss'
 })
 export class LateralDashboardComponent {
+  selectedComponent: string = 'inicio';
+  private subscription: Subscription;
 
+  constructor (private menuService: MenuServiceService) {
+    this.subscription = this.menuService.selectedOption$.subscribe(
+      selectedOption => {
+        this.selectedComponent = selectedOption;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+  
 }
