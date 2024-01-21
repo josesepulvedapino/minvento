430f05ace5b6d18a7f66b64c8fb83d4e051648d0
Jose Sepulveda <josealejandrosepulvedapino@gmail.com>
2024-01-20 22:01:39 -0300

    feat: ordenamiento por fecha precio y cantidad

--- src/components/lateral-dashboard/inventario/inventario.component.ts
+++ src/components/lateral-dashboard/inventario/inventario.component.ts
@@ -13,13 +13,16 @@ export class InventarioComponent implements OnInit {
   productos: Producto[]
   marcas: string[] = [];
   isLoaded: boolean = false;
-
   // Variable para el buscador
   searchTerm: string = '';
-
   // Variable para el select de marcas 
   selectedMarcaTerm: string = 'all';
-
+  // Variable para el ordenamiento de productos por precio
+  orderPrecioEstado: 'asc' | 'desc' | 'sinOrdenar' = 'sinOrdenar';
+  // Variable para el ordenamiento de productos por fecha
+  orderFechaEstado: 'asc' | 'desc' | 'sinOrdenar' = 'sinOrdenar';
+  // Variable para el ordenamiento de productos por cantidad
+  orderCantidadEstado: 'asc' | 'desc' | 'sinOrdenar' = 'sinOrdenar';
 
   constructor(private modalService: NgbModal, private productosService: ProductosService) {
     this.productos = [];
@@ -65,13 +68,65 @@ export class InventarioComponent implements OnInit {
       this.productosService.searchProductosByMarca(this.selectedMarcaTerm).subscribe(productos => {
         this.productos = productos;
       });
-    }
+  }
 
   searchProductosConFiltro() {
     // Obtener los productos con filtros de búsqueda por nombre y/o marca
     this.productosService.getProductosConFiltros(this.searchTerm, this.selectedMarcaTerm).subscribe(productos => {
       this.productos = productos;
     });
+    this.orderFechaEstado = 'sinOrdenar';
+    this.orderPrecioEstado = 'sinOrdenar';
+    this.orderCantidadEstado = 'sinOrdenar';
+  }
+
+  orderProductosPorPrecio() {
+    if (this.orderPrecioEstado === 'sinOrdenar') {
+      this.orderPrecioEstado = 'asc';
+      this.productos.sort((a, b) => a.precio - b.precio);
+    } else if (this.orderPrecioEstado === 'asc') {
+      this.orderPrecioEstado = 'desc';
+      this.productos.sort((a, b) => b.precio - a.precio);
+    } else if (this.orderPrecioEstado === 'desc') {
+      this.orderPrecioEstado = 'sinOrdenar';
+    }
+  }
+
+  orderProductosPorCantidad() {
+    if (this.orderCantidadEstado === 'sinOrdenar') {
+      this.orderCantidadEstado = 'asc';
+      this.productos.sort((a, b) => a.cantidad - b.cantidad);
+    } else if (this.orderCantidadEstado === 'asc') {
+      this.orderCantidadEstado = 'desc';
+      this.productos.sort((a, b) => b.cantidad - a.cantidad);
+    } else if (this.orderCantidadEstado === 'desc') {
+      this.orderCantidadEstado = 'sinOrdenar';
+    }
+  }
+
+  orderProductosPorFecha() {
+    if (this.orderFechaEstado === 'sinOrdenar') {
+      this.orderFechaEstado = 'asc';
+      this.productos.sort((a, b) => this.compararFechas(a.fecha_vencimiento, b.fecha_vencimiento));
+    } else if (this.orderFechaEstado === 'asc') {
+      this.orderFechaEstado = 'desc';
+      this.productos.sort((a, b) => this.compararFechas(b.fecha_vencimiento, a.fecha_vencimiento));
+    } else if (this.orderFechaEstado === 'desc') {
+      this.orderFechaEstado = 'sinOrdenar';
+    }
+  }
+  
+  compararFechas(fechaA: any, fechaB: any): number {
+    const dateA = new Date(fechaA.year, fechaA.month - 1, fechaA.day);
+    const dateB = new Date(fechaB.year, fechaB.month - 1, fechaB.day);
+  
+    if (dateA < dateB) {
+      return -1;
+    } else if (dateA > dateB) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 
   actualizarProductos() {
